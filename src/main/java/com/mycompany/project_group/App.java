15365827a10d9f64e661ca5017084f66fdac0855
rk8586 <ramakrishnavarre@gmail.com>
2023-07-08 13:41:19 +0530

    Update App.java

--- src/main/java/com/mycompany/project_group/App.java
+++ src/main/java/com/mycompany/project_group/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Welcome to Devops World!" );
+        System.out.println( "Welcome to the world of Jenkins" );
     }
 }
